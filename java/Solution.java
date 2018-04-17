9b40574ffec2fea1408ae12e93c2858965dd4f93
liang7878 <chengliangshi@whu.edu.cn>
2018-04-17 10:09:36 +0800

    [save] search in 2d arrry(java)

--- java/Solution.java
+++ java/Solution.java
@@ -2,25 +2,29 @@ import java.util.*;
 
 public class Solution {
     public static void main(String args[]) {
-        int[][] arrar = {{1,2,3,4,5}, {6,7,8,10,12},{13,16,17,18,19}};
+        int[][] arrar = {{1,2,8,9}, {2,4,9,12},{4,7,10,13}, {6,8,11,15}};
 
-        System.out.println(Find(10, arrar));
+        System.out.println(Find(16, arrar));
     }
 
-    public static boolean Find(int target, int[][] arrar) {
-        int x_length = arrar.length;
-        int y_length = arrar[0].length;
+    public static boolean Find(int target, int[][] arr) {
+        int x_length = arr.length;
+        int y_length = arr[0].length;
 
-        for(int i=x_length-1; i>=0; i--) {
-            for(int j=y_length-1; j>=0; j--) {
-                if(arrar[i][j] == target) {
-                    return true;
-                }
+        for(int i=x_length-1, j=0; i>=0&&j<y_length;) {
+            if(arr[i][j]==target) {
+                return true;
+            }
 
-                if(arrar[i][j]>target) {
-                    
-                }
+            if(arr[i][j]>target) {
+                i--;
+                continue;
+            }
+
+            if(arr[i][j]<target) {
+                j++;
             }
         }
+        return false;
     }
 }
