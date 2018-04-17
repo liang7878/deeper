4166089d0ebd17a1abc30219c4ef1b652f43768b
Chengliang Shi <chengliangshi@whu.edu.cn>
2018-04-17 12:44:30 +0800

    [save] add README.md

---
+++ c/io.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Enter two nums:" <<std::endl;
+    int v1=0, v2=0;
+
+    std::cin >> v1 >> v2;
+
+    std::cout << "result:" << v1 << "+" << v2 << "=" << v1+v2 <<std::endl;
+
+    return 0;
+}
